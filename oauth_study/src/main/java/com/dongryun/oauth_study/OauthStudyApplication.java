784aa6a97427d2aef3673363c1bc94266923e936
dyjung123 <jeongdongryun@gmail.com>
2021-08-29 21:36:21 +0900

    springboot study first commit

---
+++ oauth_study/src/main/java/com/dongryun/oauth_study/OauthStudyApplication.java
@@ -0,0 +1,13 @@
+package com.dongryun.oauth_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthStudyApplication.class, args);
+	}
+
+}
---
+++ oauth_study/src/test/java/com/dongryun/oauth_study/OauthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongryun.oauth_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
