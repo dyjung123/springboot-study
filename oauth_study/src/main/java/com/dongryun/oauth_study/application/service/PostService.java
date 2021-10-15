8535206f1528bec839577b5f8c78850488e03890
dyjung123 <jeongdongryun@gmail.com>
2021-10-15 23:56:34 +0900

    Create mybatis mapper xml

--- oauth_study/src/main/java/com/dongryun/oauth_study/application/service/PostService.java
+++ oauth_study/src/main/java/com/dongryun/oauth_study/application/service/PostService.java
@@ -1,7 +1,7 @@
 package com.dongryun.oauth_study.application.service;
 
 import com.dongryun.oauth_study.infrastructure.entity.Post;
-import com.dongryun.oauth_study.repository.PostRepository;
+import com.dongryun.oauth_study.infrastructure.datasource.PostRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- oauth_study/src/main/java/com/dongryun/oauth_study/infrastructure/datasource/PostRepository.java
+++ oauth_study/src/main/java/com/dongryun/oauth_study/infrastructure/datasource/PostRepository.java
@@ -1,4 +1,4 @@
-package com.dongryun.oauth_study.repository;
+package com.dongryun.oauth_study.infrastructure.datasource;
 
 import org.apache.ibatis.annotations.Mapper;
 
