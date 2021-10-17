40610978049638a04ef6d2bc875079458c70ca65
dyjung123 <jeongdongryun@gmail.com>
2021-10-18 00:45:41 +0900

    Modify sql generator perl file

--- misc/createInsertQuery.pl
+++ misc/createInsertQuery.pl
@@ -3,6 +3,6 @@
 use strict;
 use warnings;
 
-for (my $i = 0; $i < 26000; $i++) {
-    print "INSERT INTO post (username, password, content, name) VALUES ('user%d', 'password%d', 'content%d', 'name%d')", $i, $i, $i, $i;
+for (my $i = 3664; $i < 5664; $i++) {
+    printf "INSERT INTO post (username, password, content, name) VALUES ('user%d', 'password%d', 'content%d', 'name%d');\n", $i, $i, $i, $i;
 }
