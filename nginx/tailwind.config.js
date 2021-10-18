6c21bb99d844359531330b7b7e71c57a7afe98b2
dyjung123 <jeongdongryun@gmail.com>
2021-10-18 21:18:26 +0900

    Add tailwind css layout and section style components

--- nginx/tailwind.config.js
+++ nginx/tailwind.config.js
@@ -13,7 +13,17 @@ module.exports = {
     },
     extend: {
       colors: {
-        bggray: '#F3F3F4'
+        bggray: '#F3F3F4',
+        green: '#059669'
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '60px': '60px',
+      },
+      width: {
+        '1000': '1000px',
       }
     }
   }
